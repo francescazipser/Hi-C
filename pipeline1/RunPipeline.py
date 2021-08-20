b5125a21815e63a5a878bc33b80756b89c096d29
francescazipser <76665812+francescazipser@users.noreply.github.com>
2021-08-20 10:31:19 -0700

    Update RunPipeline.py

--- pipeline1/RunPipeline.py
+++ pipeline1/RunPipeline.py
@@ -1,7 +1,13 @@
 import map_promoter_gene_ID as mp
 
-mp.map_promoter_gene_ID('<path to gene annotations table .txt file from UCSC database https://genome.ucsc.edu/cgi-bin/hgTables', 
-	'<path to NCBI gene ID table .txt file from Ensembl http://uswest.ensembl.org/index.html>',
+mp.map_promoter_gene_ID('<path to gene annotations table>', 
+	'<path to NCBI gene ID table>',
 	'<output file name for the promoter-to-gene file',
-	'<path to NCBI gene ID table .txt file from Ensembl http://uswest.ensembl.org/index.html>' + 'tabs',
+	'<path to NCBI gene ID table>' + 'tabs',
 	)
+# gene annotations table (txt file) -- gene annotations table from UCSC database https://genome.ucsc.edu/cgi-bin/hgTables. 
+#	Settings -- settings Mammal, Human, GRCh38/hg38, Gene and Gene Predictions, NCBI RefSeq, RefSeq Curated (ncbiRefSeqCurated), 
+#	region:genome, filter = NM_* 
+# path to NCBI gene ID table (txt file) -- NCBI gene IDs from Go Ensembl http://uswest.ensembl.org/index.html 
+#	Instructions: Go to BioMart, choose database 'Ensembl Genes 104', choose dataset 'human genes GRCh38.p13', 
+#	click on Attributes and select 'Gene name','RefSeq mRNA ID', 'strand',and 'NCBI gene (formerly Entrezgene) ID'. Then download the file.
