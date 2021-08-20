7f24574d99b02539779d49ca78cae71e503bae36
francescazipser <76665812+francescazipser@users.noreply.github.com>
2021-08-20 10:20:10 -0700

    Update pipeline.py

--- pipeline1/pipeline.py
+++ pipeline1/pipeline.py
@@ -3,5 +3,5 @@ import map_promoter_gene_ID as mp
 mp.map_promoter_gene_ID('<path to gene annotations table .txt file from UCSC database https://genome.ucsc.edu/cgi-bin/hgTables', 
 	'<path to NCBI gene ID table .txt file from Ensembl http://uswest.ensembl.org/index.html>',
 	'<output file name for the promoter-to-gene file',
-	'<path to NCBI gene ID table .txt file from Ensembl http://uswest.ensembl.org/index.html> + tabs',
+	'<path to NCBI gene ID table .txt file from Ensembl http://uswest.ensembl.org/index.html>' + 'tabs',
 	)
