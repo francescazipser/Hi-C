1574b4de9878cfca69bd705c41e30e1b89315080
francescazipser <76665812+francescazipser@users.noreply.github.com>
2021-08-20 10:39:20 -0700

    Update remove_duplicates.py

--- pipeline1/remove_duplicates.py
+++ pipeline1/remove_duplicates.py
@@ -6,7 +6,7 @@ def remove_duplicates(filename):
 	duplicated_transcripts = anno_plain.loc[anno_plain.duplicated(subset=['name'],keep=False)].copy()
 	duplicated_transcripts_nofirst = anno_plain.loc[anno_plain.duplicated(subset=['name'])].copy()
 	num_duplic = duplicated_transcripts.groupby(duplicated_transcripts.name.tolist(),as_index=False).size()
-	duplic_names = anno_plain.set_index('name').loc[num_duplic.index.tolist()].reset_index()
+	duplic_names = anno_plain.set_index('name').loc[num_duplic['index'].tolist()].reset_index()
 	my_selec = duplic_names[duplic_names['chrom'].map(lambda x: (x[-1].isdigit()) | (x[-1]=='X'))]
 	rows_to_remove = duplic_names.merge(my_selec,how='left', indicator=True).pipe(lambda df:df.loc[df._merge=='left_only'])
 	final_anno = anno_plain[rows_to_remove.columns[:-1]].merge(rows_to_remove.iloc[:,:-1],how='left', indicator=True).pipe(lambda df:df.loc[df._merge=='left_only']).iloc[:,:-1]
